1a51e961a0a25888aaaa0b397b68c1d3673c73e1
Shodan <mega@ioexception.at>
2015-04-06 21:50:08 +0200

    debug by default

--- config.example.py
+++ config.example.py
@@ -3,7 +3,7 @@ database_uri = 'mysql://root@localhost/pushjet_api'
 
 # Are we debugging the server?
 # Do not turn this on when in production!
-debug = False
+debug = True
 
 # Limit requests?
 limiter = False
