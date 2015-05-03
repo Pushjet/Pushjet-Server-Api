dd384c7b16f539513182023fc3f2b8ba36dc8938
Shodan <mega@ioexception.at>
2015-05-03 16:50:05 +0200

    Reformat database

--- database.sql
+++ database.sql
@@ -19,11 +19,12 @@ CREATE TABLE IF NOT EXISTS `pushjet_api`.`listen` (
   `device`            VARCHAR(40)      NOT NULL,
   `service_id`        INT(10) UNSIGNED NOT NULL,
   `timestamp_created` TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `timestamp_checked` TIMESTAMP        NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
+  `timestamp_checked` TIMESTAMP            NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
   ENGINE = InnoDB
-  DEFAULT CHARSET =utf8
-  COLLATE =utf8_unicode_ci;
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci;
 
 
 -- -----------------------------------------------------
@@ -35,14 +36,15 @@ CREATE TABLE IF NOT EXISTS `pushjet_api`.`message` (
   `id`                INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `service_id`        INT(10) UNSIGNED NOT NULL,
   `text`              TEXT             NOT NULL,
-  `title`             VARCHAR(255)     NULL DEFAULT NULL,
+  `title`             VARCHAR(255)         NULL  DEFAULT NULL,
   `level`             TINYINT(4)       NOT NULL DEFAULT '0',
-  `link`              TEXT             NULL,
+  `link`              TEXT                 NULL,
   `timestamp_created` TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`)
+)
   ENGINE = InnoDB
-  DEFAULT CHARSET =utf8
-  COLLATE =utf8_unicode_ci;
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci;
 
 -- -----------------------------------------------------
 -- Table `pushjet_api`.`service`
@@ -54,12 +56,13 @@ CREATE TABLE IF NOT EXISTS `pushjet_api`.`service` (
   `secret`            VARCHAR(32)      NOT NULL,
   `public`            VARCHAR(40)      NOT NULL,
   `name`              VARCHAR(255)     NOT NULL,
-  `icon`              TEXT             NULL,
+  `icon`              TEXT                 NULL,
   `timestamp_created` TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`)
+)
   ENGINE = InnoDB
-  DEFAULT CHARSET =utf8
-  COLLATE =utf8_unicode_ci;
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci;
 
 -- -----------------------------------------------------
 -- Table `pushjet_api`.`gcm`
@@ -71,10 +74,11 @@ CREATE TABLE IF NOT EXISTS `pushjet_api`.`gcm` (
   `uuid`              VARCHAR(40)      NOT NULL,
   `gcmid`             TEXT             NOT NULL,
   `timestamp_created` TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`)
+)
   ENGINE = InnoDB
-  DEFAULT CHARSET =utf8
-  COLLATE =utf8_unicode_ci;
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci;
 
 SET SQL_MODE = @OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
